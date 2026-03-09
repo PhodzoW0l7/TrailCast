4ce64bcce196b596258c2a7ebac817d45302b711
PhodzoW0l7 <Phodzonagz12@gmail.com>
2026-03-10 00:35:07 +0200

    Created TrailCast repo

---
+++ Backend/src/main/java/com/trailcast/TrailCast/TrailCastApplication.java
@@ -0,0 +1,13 @@
+package com.trailcast.TrailCast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailCastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailCastApplication.class, args);
+	}
+
+}
